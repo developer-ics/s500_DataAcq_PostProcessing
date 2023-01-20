63ca786d249304dadd21a612d22ab0a02a697ae7
developer-ics <123088788+developer-ics@users.noreply.github.com>
2023-01-20 09:44:58 -0500

    Add files via upload
    Signed-off-by: developer-ics <123088788+developer-ics@users.noreply.github.com>

--- PostProcessing/read_emlid_LLH_raw.m
+++ PostProcessing/read_emlid_LLH_raw.m
@@ -20,22 +20,27 @@ dirnm=['.\YF2Reachm2_raw_' fs '_RINEX_3_03']
 
 
 dd=dir(dirnm)
-fn=[dirnm '\YF2Reachm2_raw_' fs '.pos.txt']% note the e-file has a slightly modifed header to allow matalab to read the varaible names
-T2=readtable(fn,'FileType','text');
-T2.date=datetime(T2.GPST,'Inputformat','yyyy/MM/dd');
-T2.datetime=T2.date+T2.time;
+fn=[dirnm '\YF2Reachm2_raw_' fs '.pos']% note the e-file has a slightly modifed header to allow matalab to read the varaible names
+opts = detectImportOptions(fn,'FileType','text');
+opts.DataLines=[11 Inf];
+opts.VariableNamesLine=10;
+
+T2=readtable(fn,opts);
+T2 = renamevars(T2,'x_','Date')
+T2.date=datetime(T2.Date,'Inputformat','yyyy/MM/dd');
+T2.datetime=T2.date+T2.GPST;
 datetime_ppk=T2.datetime;
-lat_ppk=T2.latitude;
-lon_ppk=T2.longitude;
+lat_ppk=T2.latitude_deg_;
+lon_ppk=T2.longitude_deg_;
 figure(10);
 hold on
 plot(lon_ppk,lat_ppk,'.-g')
 
 figure(11);clf
-height_ell_ppk=T2.height;
-plot(T2.height)
+height_ell_ppk=T2.height_m_;
+plot(T2.height_m_)
 hold on
 plot(10*T2.Q)
 
-plot(10000*(T2.latitude-T2.latitude(1)))
+plot(10000*(T2.latitude_deg_-T2.latitude_deg_(1)))
 save(['PPK' fs],'lon_ppk','lat_ppk','height_ell_ppk','datetime_ppk','lat','lon','datetime1')

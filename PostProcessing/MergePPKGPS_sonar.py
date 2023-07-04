f6da24c576ae1aa82733c83d6170e513f849ca75
spike <spicer.bak.frf@gmail.com>
2023-07-04 14:57:44 -0400

    made duplicate of read emlid LLH_raw matlab file " " ;
    ;

---
+++ PostProcessing/MergePPKGPS_sonar.py
@@ -0,0 +1,188 @@
+
+% MERGEGPS_SONAR Summary of this function goes here
+dirstr='01-11-2023'
+dirstr='12-21-2022' % with emlid ppk GPS saved and processed
+dirstr='01-05-2023'
+
+GPS=load(['nmea' dirstr '.mat'])
+
+PPKGPS=load('PPK20230105141705.mat');
+
+%% compare gps data to make sure timing is ok
+figure(1);clf
+plot(GPS.gps_time)
+hr=floor(GPS.gps_time(:)/10000);
+mm=floor(rem(GPS.gps_time(:)./10000,1).*100);
+ss=rem(GPS.gps_time(:)./100,1).*100;
+GPS.mat_time=datetime(str2num(dirstr(end-3:end)),str2num(dirstr(1:2)),str2num(dirstr(4:5)),...
+    hr,mm,ss);
+
+pc_time_off=GPS.pc_time_gga(1)-GPS.mat_time(1)
+
+plot(GPS.mat_time,GPS.lat,'.')
+hold on
+gps_leap_offset=seconds(18);%should be 19 but 18 lines it up!
+plot(PPKGPS.datetime1-gps_leap_offset,PPKGPS.lat,'r')
+plot(PPKGPS.datetime_ppk-gps_leap_offset,PPKGPS.lat_ppk,'g')
+
+
+%%
+
+if 0 % use the matlab bed deection from profile data
+S=load(['profile_data' dirstr '.mat'])
+sonar_time=S.dt_profile
+sonar_range=S.bed_detect_range';
+end
+
+if 1 % use the cerulean instantaenous bed detection since not sure about delay with smoothed 
+S1=load(['detected_range' dirstr '.mat'])
+sonar_time=S1.dt_txt(:)+hours(5);%convert to UTC
+sonar_range=S1.txt_depth(:);
+end
+%%
+figure(2);clf
+plot(sonar_time,sonar_range,'b')
+hold on
+plot(GPS.mat_time,GPS.altWGS84,'.g')
+hold on 
+ppk_gpstime=PPKGPS.datetime_ppk-gps_leap_offset;
+plot(ppk_gpstime,PPKGPS.height_ell_ppk,'.r')
+
+figure(3);clf
+subplot(211)
+plot(sonar_range)
+
+subplot(212)
+inds=(900:5210)% need to set this manually for each data set
+plot(sonar_range(inds))
+indsp=near(ppk_gpstime,sonar_time(inds(1))):near(ppk_gpstime,sonar_time(inds(end)));
+%%
+
+% code gps
+%sonar_range_i=interp1(sonar_time,sonar_range,GPS.pc_time_gga(inds));
+%lati=GPS.lat(inds);loni=GPS.lon(inds);
+
+% ppk gps
+sonar_range_i=interp1(sonar_time,sonar_range,ppk_gpstime(indsp));
+lati=PPKGPS.lat_ppk(indsp);loni=PPKGPS.lon_ppk(indsp);
+
+
+%% another time sync here by correlating time
+figure(40);clf
+ws=2100:2400 ;% pick a nice section with a heave effects visble in the echodata and gps
+
+ws2=2000:2500 ;% 
+% this data doesn't have it so adding artficially 
+csonar_range_i=sonar_range_i;
+csonar_range_i(ws)=csonar_range_i(ws)+.3*sin(ws/10)';
+
+ssr=detrend(csonar_range_i(ws2),'const')+.3*sin(ws2/10)'
+hold on
+chc=PPKGPS.height_ell_ppk(indsp);
+shift=0*pi/4
+chc(ws)=(chc(ws)+.3*sin(ws/10+shift)');
+
+sdhc=detrend(chc(ws2)+.3*sin(ws2/10+shift)','const')
+subplot(412)
+plot(sdhc);
+hold on
+plot(ssr);
+subplot(411)
+plot(csonar_range_i)
+hold on
+plot(chc)
+
+
+subplot(413)
+[r,lags]=xcorr(ssr,sdhc,200,'coeff');
+plot(lags,r)
+
+[mx,mi]=max(r);
+ml=lags(mi)
+s_ssr=interp1(1:length(ssr),ssr,[1:length(ssr) ]+ml);
+shifted_sonar_range_i=interp1(1:length(sonar_range_i),sonar_range_i,[1:length(sonar_range_i) ]+ml);
+
+subplot(414)
+plot(sdhc);
+hold on
+plot(s_ssr);
+%%
+figure(4) ;clf
+scatter(loni,lati,16,sonar_range_i)
+cc=turbo;
+cc=flipud(cc);
+colormap(cc)
+colorbar
+hold on
+caxis([7 13])
+
+%%
+antenna_offset=.25;
+x=loni(:);
+y=lati(:);
+z=-sonar_range_i(:)+PPKGPS.height_ell_ppk(indsp)-antenna_offset;
+dx=.000005;
+xnodes=(min(x)-dx):dx:(max(x)+dx);
+ynodes=(min(y)-dx):dx:(max(y)+dx);
+sff= 2e-04;
+[Zg3,Xg3,Yg3]=RegularizeData3D(x ,y,z,xnodes,ynodes,'smoothness',sff);
+% RegularizeData3D needs to be downloaded from the matlab file exchange
+%%
+figure(22);clf
+scatter3(x,y,z,12,z,'filled');
+view(2)
+hold on
+% hold on
+% sl=600
+% for ii=1:(floor(length(x)/sl)-3)
+%     fit_inds=[1:sl]+sl+sl*ii
+%     scatter(x(fit_inds),y(fit_inds),12,z(fit_inds),'or');
+%     pause(.1)
+% end
+k=boundary(x,y,.3);%x and y are track line coordinate vectors in UTM, z is the surface re navd88
+plot(x(k),y(k),'k','linewidth',3)
+[m,n]=size(Zg3)
+BW=roipoly(Xg3(1,:),Yg3(:,1),Zg3,x(k),y(k));
+BW=double(BW);
+BW(BW==0)=NaN;
+
+
+DEM=GRIDobj(Xg3,Yg3,Zg3.*BW);
+set(gca,'dataaspectratio',[1 1 1])
+
+
+%%
+figure(31);clf
+cm=(ttscm('broc'));
+%cm=ttcmap('etopo1')
+wgsz=32
+  dataMax = -1-wgsz;
+  dataMin = -14-wgsz;
+  centerPoint = -10-wgsz;
+  scalingIntensity = 4;
+%Then perform some operations to create your colormap. I have done this by altering the indices “x” at which each existing color lives, and then interpolating to expand or shrink certain areas of the spectrum.
+  xm = 1:length(cm); 
+  xm = xm - (centerPoint-dataMin)*length(xm)/(dataMax-dataMin);
+  xm = scalingIntensity * xm/max(abs(xm));
+%Next, select some function or operations to transform the original linear indices into nonlinear. In the last line, I then use “interp1” to create the new colormap from the original colormap and the transformed indices.
+  xm = sign(xm).* exp(abs(xm));
+  xm = xm - min(xm); xm = xm*511/max(xm)+1; 
+  ncm = interp1(xm, cm, 1:512);
+
+%contour(DEM,[],'caxis',[-18 -7],'colorbar',1,'colormap',cm,'ticklabels','nice','exaggerate',1,...
+ %   'colorbarylabel','Elevation (M)')
+tanakacontour(DEM,[-14:.25:-2]-wgsz)
+%caxis([-14 -3]-wgsz)
+title('Yellow Fin survey')
+hold on 
+%plot(x,y,'.k');
+sf =10;
+hs=scatter(x(1:sf:end),y(1:sf:end),12,z(1:sf:end),'o')
+hs.MarkerEdgeColor='k'
+%cc=flipud(cc);
+colormap(ncm)
+hc=colorbar
+hc.Label.String='Depth (m)'
+xlabel('Lon (Deg)')
+ylabel('Lat (Deg)')
+print -dpng goodwill_pond
--- PostProcessing/read_emlid_LLH_raw.py
+++ PostProcessing/read_emlid_LLH_raw.py
@@ -2,10 +2,12 @@ import os
 import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
-
+import glob
 ## converted read_emlid_LLH_raw.m in chatGPT on 19 February 2023
 
-#Note that in Python, the code needs to explicitly import the necessary libraries and functions, unlike MATLAB which has them preloaded. Additionally, Python uses different syntax and naming conventions compared to MATLAB, so some modifications were made to convert the code.
+#Note that in Python, the code needs to explicitly import the necessary libraries and functions,
+# unlike MATLAB which has them preloaded. Additionally, Python uses different syntax and naming
+# conventions compared to MATLAB, so some modifications were made to convert the code.
 # Emlid's software (free) emlid studio
 # ubx -> rinex
 # Will ask for base station file too (Should be also rinex) then it'll do processing and spit out text file
@@ -13,45 +15,76 @@ import matplotlib.pyplot as plt
 # Rinex files go to emlid studio
 # pos file is output from emlid studio file (text file)
 #
+#
+# fs = '20221221165017'
+# fs = '20230105141705'
+folderstr = 20230327
+rootDir = f'/data/yellowfin/{folderstr}'
+# find folders that have name rinex in it and aren't zip files (these contain the pos files)
+fldrlistLLH, fldrlistPPK = [], []
+[fldrlistPPK.append(fname)  for fname in os.listdir(rootDir) if 'RINEX' in fname and '.zip' not in fname]
+[fldrlistLLH.append(fname)  for fname in os.listdir(rootDir) if 'LLH' in fname and '.zip' not in fname]
 
-fs = '20221221165017'
-fs = '20230105141705'
-dirnm = f'YF2Reachm2_solution_{fs}_LLH'
 
-dd = os.listdir(dirnm)
-fn = os.path.join(dirnm, dd[2])  # this is before ppk processing so should agree with nmea strings
-T = pd.read_csv(fn, delimiter='\s+', header=None)
-T.columns = ['Var1', 'Var2', 'Var3', 'Var4']
-T['date'] = pd.to_datetime(T['Var1'], format='%Y/%m/%d')
-T['datetime'] = T['date'] + pd.to_timedelta(T['Var2'])
-datetime1 = T['datetime']
-lat = T['Var3']
-lon = T['Var4']
-fig = plt.figure(10)
-fig.clf()
-plt.plot(lon, lat, 'o-m')
+# first load the LLH quick processed data
+T_LLH = pd.DataFrame()
+for fldr in sorted(fldrlistLLH):
+    # this is before ppk processing so should agree with nmea strings
+    fn = glob.glob(os.path.join(rootDir, fldr, "*"))[0]
+    try:
+        T = pd.read_csv(fn, delimiter='  ', header=None, engine='python')
+        print(f'loaded {fn}')
+        if all(T.iloc[-1]):  #if theres nan's in the last row
+            T = T.iloc[:-1] # remove last row
+        T_LLH = pd.concat([T_LLH, T]) # merge multiple files to single dataframe
+        
+    except:
+        continue
 
-dirnm = f'.\\YF2Reachm2_raw_{fs}_RINEX_3_03'
+T_LLH['datetime'] = pd.to_datetime(T_LLH[0], format='%Y/%m/%d %H:%M:%S.%f')
+T_LLH['lat'] =  T_LLH[1]
+T_LLH['lon'] = T_LLH[2]
 
-dd = os.listdir(dirnm)
-fn = os.path.join(dirnm, 'YF2Reachm2_raw_' + fs + '.pos')  # note the e-file has a slightly modifed header to allow matlab to read the variable names
-T2 = pd.read_csv(fn, skiprows=10)
-T2.rename(columns={'x_': 'Date'}, inplace=True)
-T2['date'] = pd.to_datetime(T2['Date'], format='%Y/%m/%d')
-T2['datetime'] = T2['date'] + pd.to_timedelta(T2['GPST'])
-datetime_ppk = T2['datetime']
-lat_ppk = T2['latitude_deg_']
-lon_ppk = T2['longitude_deg_']
-fig = plt.figure(10)
-plt.plot(lon_ppk, lat_ppk, '.-g')
 
-fig = plt.figure(11)
-fig.clf()
-height_ell_ppk = T2['height_m_']
-plt.plot(height_ell_ppk)
-plt.hold(True)
-plt.plot(10 * T2['Q'])
-plt.plot(10000 * (T2['latitude_deg_'] - T2['latitude_deg_'].iloc[0]))
-plt.savefig(f'PPK{fs}.png')
+# now load  Post processed kinematic PPK files from the pos file that was post processed in emlid
+T_ppk = pd.DataFrame()
+for fldr in sorted(fldrlistPPK):
+    # this is before ppk processing so should agree with nmea strings
+    fn = glob.glob(os.path.join(rootDir, fldr, "*.pos"))[0]
+    try:
+        colNames = ['datetime', 'lat', 'lon', 'height', 'Q', 'ns', 'sdn(m)',  'sde(m)', 'sdu(m)', \
+                    'sdne(m)', 'sdeu(m)',  'sdun(m)', 'age(s)',  'ratio']
+        Tpos = pd.read_csv(fn, delimiter=r'\s+ ', header=10, names=colNames, engine='python')
+        print(f'loaded {fn}')
+        if all(Tpos.iloc[-1]):  #if theres nan's in the last row
+            Tpos = Tpos.iloc[:-1] # remove last row
+        T_ppk = pd.concat([T_ppk, Tpos]) # merge multiple files to single dataframe
+        
+    except:
+        continue
+T_ppk['datetime'] = pd.to_datetime(T_ppk['datetime'], format='%Y/%m/%d %H:%M:%S.%f')
+
+
+# now make plot of both files
+# first llh file
+plt.plot(T_LLH['lon'], T_LLH['lat'], '.-m', label = 'LLH file')
+plt.xlabel('longitude')
+plt.ylabel('latitude')
+# fir
+plt.plot(T_ppk['lon'], T_ppk['lat'], '.-g', label = 'PPK file')
+plt.xlabel('longitude')
+plt.ylabel('latitude')
+plt.legend()
+plt.tight_layout()
+
+
+fig = plt.figure()
+plt.plot(T_ppk['datetime'], T_ppk['height'], label='elevation')
+plt.plot(T_ppk['datetime'], 10 * T_ppk['Q'], '.', label='quality factor')
+plt.plot(T_ppk['datetime'], 10000 * (T_ppk['lat'] - T_ppk['lat'].iloc[0]), label='lat from original lat')
+plt.xlabel('time')
+plt.legend()
+plt.tight_layout()
+# plt.savefig(f'PPK{fs}.png')
 
 T2.to_csv(f'PPK{fs}.csv', index=False)
